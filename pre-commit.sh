a208321921d622b2c03fdef4dfc6a1d35ecfd75a
谭海洋 <haiyang_tan@sui.com>
2018-07-20 15:16:07 +0800

    添加 commit 时检测脚本

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,6 +2,23 @@
 
 REPO_ROOT_DIR="$(git rev-parse --show-toplevel)"
 
-files=$(git ls-files -m | grep -Ei "\.java")
+files=$(git diff --name-only --diff-filter=ACMRTUXB HEAD~0)
 
-echo ${files}
+#echo ${files}
+
+./gradlew LintIncrementTaskDebug
+
+OUTPUT=$(./gradlew LintIncrementTaskDebug | grep "Lint found" 2>&1)
+
+ERROR=$(echo $OUTPUT|grep -o '[0-9]\+' 2>&1)
+
+ARR=($ERROR)
+
+for DATA in ${ARR[@]}
+do
+    if [ "$DATA" -ne 0 ];
+    then
+        open "${REPO_ROOT_DIR}/app/build/reports/lint-results-debug.html"
+        exit 1
+    fi
+done
